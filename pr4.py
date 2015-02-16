2c0c939d89941a82f7046493d91e3f0c16399a0e
hollandlive <a.aksenovs@gmail.com>
2015-02-16 10:32:18 +0100

    extra salary
    my python program salary 1.0. program calculates ur weekly salary. it
    pays you 1.5 times extra money for extra hours (above 40 hours)

---
+++ pr4.py
@@ -0,0 +1,17 @@
+#my python program salary 1.0. program calculates ur weekly salary. it pays you 1.5 times extra money for extra hours (above 40 hours)
+print 'Step 2\n', 'I gonna calculate ur salary,\n'
+hours = raw_input('Enter hours u worked this week\n')
+hours = float(hours)
+rate = raw_input('Enter ur rate\n')
+rate = float(rate)
+print 'your hours worked: \n', hours, '\n', 'ur rate is: \n', rate, ' per hour\n'
+if hours <= 40:
+    sal = hours * rate
+    print 'ur salary this week is ', sal
+else: 
+    hours_extra = hours - 40
+    rate_extra = rate * 1.5
+    sal = hours * rate
+    sal_extra = hours_extra * rate_extra
+    sal = sal + sal_extra
+print 'congrats Mr\n, u worked ', hours, '\n', ' ur salary is \n', sal
